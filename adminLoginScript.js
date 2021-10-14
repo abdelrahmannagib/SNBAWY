e80b559142f37e42fe147d4904ba74f1c4d15bfe
abelrahman nagib <abdonagib204@gmail.com>
2021-10-15 01:55:23 +0200

    change pass

--- adminLoginScript.js
+++ adminLoginScript.js
@@ -1,7 +1,7 @@
 function senddd() {
     var  username=document.getElementById("idd").value;
    var kk=document.getElementById("passs").value;
-   if(username=="admin"&&kk=="12345")
+   if(username=="admin"&&kk=="123456")
    {
     setTimeout(function() {window.location = "adminPage.html" });
    }
